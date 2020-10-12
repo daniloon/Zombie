9670efda5859a78fa13e9376941aab0315586535
ibyrd96 <icybyrd96@gmail.com>
2020-10-12 14:31:17 -0600

    Stashed Changes

--- Zombie Cakes/Assets/Menu/MenuCentral.cs
+++ Zombie Cakes/Assets/Menu/MenuCentral.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MenuCentral : MonoBehaviour {
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CreditButton()
+    {
+        SceneManager.LoadScene(9);
     }
 
     public void QuitGame()
