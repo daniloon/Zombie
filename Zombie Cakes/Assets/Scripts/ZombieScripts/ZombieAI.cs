33c603cd4305ac2c3f42cec7238b0a8e11d96fc0
WillMarda <32785881+TheRealBlujuce@users.noreply.github.com>
2020-10-12 10:29:15 -0600

    Zombies Implemented.
    I added in the rest of the current Zombie animations that I have made, as well as created a basic navmesh. The zombie has health, and a death script, but since we dont have shooting it wont die just yet but the code is there. Models are all looking good, but some issues with the transform of the Orientation object when crouching or sliding make it so that the Hand and Pistol models get distorted.

---
+++ Zombie Cakes/Assets/Scripts/ZombieScripts/ZombieAI.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieAI : MonoBehaviour
+{
+
+    // This is to access the game controller
+
+    // Zombie values
+    float health = 20;
+    int scoreValue = 100;
+    public bool canMove = true;
+    public bool wasShot = false;
+
+    // AI setup
+    NavMeshAgent navAgent;
+    GameObject myTarget;
+    public GameObject myCorpse;
+    Animator zombieAnimator;
+
+    int tick = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        navAgent = GetComponent<NavMeshAgent>();
+        myTarget = GameObject.FindGameObjectWithTag("Player");
+
+        zombieAnimator = this.GetComponent<Animator>();
+
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+
+        gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+        canMove = false;
+
+        zombieAnimator.SetTrigger("Shot");
+        health -= damageAmount;
+        //Debug.Log("I Cant Move!");
+
+        if (health <= 0)
+        {
+          Die();
+        }
+
+    }
+
+    void Die()
+    {
+
+        Instantiate(myCorpse, this.transform.position, this.transform.rotation);
+        Destroy(gameObject);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if the zombie can move, then we make it move.
+        if (canMove == true)
+        {
+            navAgent.SetDestination(myTarget.transform.position);
+        }
+
+
+
+        // if the zombie is not currently able to move, then we fix that issue and get him moving again.
+        if (wasShot == true)
+        {
+
+            tick++;
+
+            if (tick > 32 && zombieAnimator.GetCurrentAnimatorStateInfo(0).IsTag("isRunning"))
+            {
+
+                wasShot = false;
+                canMove = true;
+                //Debug.Log("I Can Move Again!");
+                gameObject.GetComponent<NavMeshAgent>().isStopped = false;
+
+            }
+
+        }
+
+    }
+
+}
