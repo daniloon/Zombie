b614ea9872d801a89bb2e0fc9370dc89b646c046
Jpmcdc <46508789+Jpmcdc@users.noreply.github.com>
2020-10-20 19:11:50 -0600

    Player Health Implemented
    - Modified Player collision box
    - Modified Zombie collision box
    - Added player health
    - Added lost health when collison with zombie is detected

---
+++ Zombie Cakes/Assets/Scripts/First Person Movement Scripts/Player Stuff/Player_Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Health : MonoBehaviour
+{
+    int health = 100;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            
+            
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collisioninfo)
+    {
+       if(collisioninfo.collider.name == "Zombie")
+        {
+            health = health - 50;
+            Debug.Log(health);
+        }
+    }
+}
