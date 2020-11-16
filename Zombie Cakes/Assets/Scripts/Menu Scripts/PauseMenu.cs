e0f122df4d9dd14282ccd5fd93fc650f6aa42639
ibyrd96 <icybyrd96@gmail.com>
2020-11-16 15:12:35 -0700

    Revert "Controls Menu and Pause Menu bug fixes"
    This reverts commit 34349bebf69d6795ceaad2a05f25c38e0f2f465f.

--- Zombie Cakes/Assets/Scripts/Menu Scripts/PauseMenu.cs
+++ Zombie Cakes/Assets/Scripts/Menu Scripts/PauseMenu.cs
@@ -11,7 +11,6 @@ public class PauseMenu : MonoBehaviour
     public UnityEvent onPause = new UnityEvent();
     public UnityEvent onResume = new UnityEvent();
     public GameObject pauseMenuUI;
-    public GameObject ControlsMenu;
 
     //Action at start of Script
     void Start()
@@ -40,7 +39,6 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
-        ControlsMenu.SetActive(false);
         Time.timeScale = 1f;
         GamePaused = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -58,27 +56,6 @@ public class PauseMenu : MonoBehaviour
         onPause.Invoke();
     }
 
-    //Gets into the main 
-    public void LoadControls()
-    {
-        if (ControlsMenu != null)
-        {
-            ControlsMenu.SetActive(true);
-            pauseMenuUI.SetActive(false);
-            Time.timeScale = 0f;
-        }
-    }
-
-    //Code for getting back to pause menu
-    public void PauseBack()
-    {
-        if (pauseMenuUI != null)
-        {
-            pauseMenuUI.SetActive(true);
-            ControlsMenu.SetActive(false);
-        }
-    }
-
     //Actions to go back to Main Menu
     public void LoadMenu()
     {
@@ -86,7 +63,7 @@ public class PauseMenu : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    // Pause Menu's "Quit" Button
+    //Code for Pause Menu's "Quit" Button
     public void QuitGame()
     {
         Debug.Log("Quit!");
