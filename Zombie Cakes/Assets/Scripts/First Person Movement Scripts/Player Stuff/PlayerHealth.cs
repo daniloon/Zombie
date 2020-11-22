74a8d081f2505369bf108c14a9f74c2b9cc2af73
ibyrd96 <icybyrd96@gmail.com>
2020-11-22 04:37:28 -0700

    Player HP Bar

---
+++ Zombie Cakes/Assets/Scripts/First Person Movement Scripts/Player Stuff/PlayerHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void OnCollisionEnter(Collision collisioninfo)
+    {
+
+        if (collisioninfo.collider.tag == "Zombie")
+        {
+            TakeDamage(10);
+            //uniDebug.Log(health);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    private void Update()
+    {
+
+        if (currentHealth <= 0)
+        {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+}
