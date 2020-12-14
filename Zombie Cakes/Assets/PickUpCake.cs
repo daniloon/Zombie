b5baf4ae9fa2ecc8dbfbd8b7538af906ed2fce0d
WillMarda <32785881+TheRealBlujuce@users.noreply.github.com>
2020-12-14 15:06:27 -0700

    Cake Fix
    JP and I fixed the cake spawning and collision of new instantiated cakes. There is a bug now where when dropping off the cake, the cake pointer does not update

--- Zombie Cakes/Assets/PickUpCake.cs
+++ Zombie Cakes/Assets/PickUpCake.cs
@@ -5,34 +5,59 @@ using UnityEngine;
 public class PickUpCake : MonoBehaviour { 
 
     public GameObject cakePrefab;
+    public cakeMechanics thisCake;
     public int randomNumber;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+  
     }
 
     // Update is called once per frame
     void Update()
     {
         // handles the event of the cake having been delivered
-        if (GameObject.Find("Cake") == null)
+        if (GameObject.FindGameObjectWithTag("Cake") == null)
         {
             // decide on store to spawn cake (random number between 1 and 3)
             randomNumber = Random.Range(1, 4);
 
             if (randomNumber == 1)
             {
-                Instantiate(cakePrefab, new Vector3(1035, 13, 94), Quaternion.identity);
+
+                var i = Instantiate(cakePrefab, new Vector3(1035, 13, 94), Quaternion.identity);
+                thisCake = i.GetComponent<cakeMechanics>();
+
+                thisCake.cakeHolder = GameObject.Find("LeftCakeHolder").GetComponent<Transform>();
+                thisCake.cakeHealthBar = GameObject.Find("CakeBar").GetComponent<CakeHealthBar>();
+                thisCake.playerAtt = GameObject.Find("RightPlayerArm").GetComponent<playerAttack>();
+                thisCake.playerMove = GameObject.Find("Player").GetComponent<PlayerMovement>();
+
             } 
             else if (randomNumber == 2)
             {
-                Instantiate(cakePrefab, new Vector3(1234, 13, 998), Quaternion.identity);
+
+                var i = Instantiate(cakePrefab, new Vector3(1234, 13, 998), Quaternion.identity);
+                thisCake = i.GetComponent<cakeMechanics>();
+
+                thisCake.cakeHolder = GameObject.Find("LeftCakeHolder").GetComponent<Transform>();
+                thisCake.cakeHealthBar = GameObject.Find("CakeBar").GetComponent<CakeHealthBar>();
+                thisCake.playerAtt = GameObject.Find("RightPlayerArm").GetComponent<playerAttack>();
+                thisCake.playerMove = GameObject.Find("Player").GetComponent<PlayerMovement>();
+
             } 
             else
             {
-                Instantiate(cakePrefab, new Vector3(167, 13 , 127), Quaternion.identity);
+
+                var i = Instantiate(cakePrefab, new Vector3(167, 13 , 127), Quaternion.identity);
+                thisCake = i.GetComponent<cakeMechanics>();
+
+                thisCake.cakeHolder = GameObject.Find("LeftCakeHolder").GetComponent<Transform>();
+                thisCake.cakeHealthBar = GameObject.Find("CakeBar").GetComponent<CakeHealthBar>();
+                thisCake.playerAtt = GameObject.Find("RightPlayerArm").GetComponent<playerAttack>();
+                thisCake.playerMove = GameObject.Find("Player").GetComponent<PlayerMovement>();
+
             }
             
         }
