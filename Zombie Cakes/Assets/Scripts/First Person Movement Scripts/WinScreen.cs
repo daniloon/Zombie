0380408aac27afd4e6ca9bef9d5d2707e34c8397
ibyrd96 <icybyrd96@gmail.com>
2020-12-16 15:19:05 -0700

    WinScreen added

---
+++ Zombie Cakes/Assets/Scripts/First Person Movement Scripts/WinScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreen : MonoBehaviour
+{
+    void OnGUI()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void RestartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
